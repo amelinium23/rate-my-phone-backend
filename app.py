eaae5f0483ec6808557cf90a41b7a994589d2524
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-02-09 19:39:36 +0100

    added gitignore

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, Response
+from flask import Response
 from .Brands.brand_routes import brands
+from . import app
 
-app = Flask(__name__)
 app.register_blueprint(brands)
 
 @app.route('/')
