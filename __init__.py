91664b2ebf877d91d4639dbbc7aadce060ed0d8b
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-02-09 21:08:33 +0100

    change to wsgi and procfile

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-from config import DevelopmentConfig
-
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig())
--- main/app.py
+++ main/app.py
@@ -1,7 +1,9 @@
-from flask import Response
+from flask import Flask, Response
 from Brands.brand_routes import brands
-from config import Config
-from . import app
+from main.config import DevelopmentConfig
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig())
 
 app.register_blueprint(brands)
 
@@ -9,5 +11,3 @@ app.register_blueprint(brands)
 def hello_page() -> Response:
     return Response('Welcome to rate my phone api!', status=200)
 
-if __name__ == '__main__':
-    app.run()
