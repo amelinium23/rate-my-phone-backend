41c5ee2177fbe7ecce8701d19d773460c4964fb0
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-02-10 19:51:53 +0100

    get by key implemented

--- Brands/brand_routes.py
+++ Brands/brand_routes.py
@@ -1,4 +1,4 @@
-from ast import Dict, List
+from typing import Dict, List
 from flask import Blueprint, Response, current_app
 import json
 import requests as r
@@ -7,8 +7,9 @@ from utils.json_utils import EnhancedJSONEncoder
 
 brands = Blueprint('brands', __name__)
 
-@brands.route('/brands')
-def get_device_list() -> Response:
+
+@brands.route('/brands', methods=['GET'])
+def get_brand_list() -> Response:
     GSM_ARENA_API_URL = current_app.config.get('GSM_ARENA_API_URL', '')
     req = r.get(GSM_ARENA_API_URL, {'route': 'brand-list'})
     data: Dict = req.json()
@@ -16,3 +17,19 @@ def get_device_list() -> Response:
     return Response(json.dumps(brands, cls=EnhancedJSONEncoder),
                     status=req.status_code,
                     content_type=req.headers.get('content-type'))
+
+
+@brands.route('/brands/<key>', methods=['GET'])
+def get_brand_key(key: str) -> Response:
+    GSM_ARENA_API_URL = current_app.config.get('GSM_ARENA_API_URL', '')
+    req = r.get(GSM_ARENA_API_URL, {'route': 'brand-list'})
+    data: Dict = req.json()
+    brands: List[Brand] = [Brand(**brand) for brand in data.get('data', {})]
+    return Response(json.dumps(get_brand_by_key(brands, key), cls=EnhancedJSONEncoder))
+
+
+def get_brand_by_key(brands: List[Brand], key: str) -> Brand:
+    for brand in brands:
+        if brand.key == key:
+            return brand
+    return None
