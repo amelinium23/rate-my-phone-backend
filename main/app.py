e34be1cdf5c58e3c3066fd2cf8205ece9893ab12
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-02-13 09:29:30 +0100

    some changed

--- main/app.py
+++ main/app.py
@@ -3,11 +3,12 @@ from brands.brand_routes import brands
 from main.config import DevelopmentConfig
 
 app = Flask(__name__)
+
 app.config.from_object(DevelopmentConfig())
 
 app.register_blueprint(brands)
 
+
 @app.route('/')
 def hello_page() -> Response:
-    return Response('Welcome to rate my phone api!', status=200)
-
+  return Response('Welcome to rate my phone api!', status=200)
