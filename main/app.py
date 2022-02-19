5d326f2f2e0d193dd254e3da5e6aa8a42b6af5d5
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-02-19 15:40:03 +0100

    added routes

--- main/app.py
+++ main/app.py
@@ -2,6 +2,7 @@ from logging import Logger
 from flask import Flask, Response
 from brands.brand_routes import BRANDS
 from device.device_routes import DEVICE
+from user.user_routes import USER
 from .config import Config
 
 app: Flask = Flask(__name__)
@@ -9,8 +10,11 @@ app: Flask = Flask(__name__)
 logger: Logger = Logger(__name__)
 
 app.config.from_object(Config())
+
 app.register_blueprint(BRANDS)
 app.register_blueprint(DEVICE)
+app.register_blueprint(USER)
+
 
 @app.route('/')
 def hello_page() -> Response:
