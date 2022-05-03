578f01e964be63b8f409c4d5887612bbe51450ea
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-05-03 11:28:32 +0200

    logging config removed

--- main/config.py
+++ main/config.py
@@ -1,9 +1,4 @@
-from logging import basicConfig
-
-config = basicConfig(format="[%(asctime)s][%(filename)s]: %(message)s")
-
 class Config:
-    DEBUG = True
-    DEVELOPMENT = True
-    GSM_ARENA_API_URL = 'https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec'
-    # LOGGER_CONFIG = config
+  DEBUG = True
+  DEVELOPMENT = True
+  GSM_ARENA_API_URL = 'https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec'
