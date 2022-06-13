9f63adb99701ae324077525b5b6561276c80cc9f
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-06-13 10:34:31 +0200

    added firebase sdk remove prints

--- utils/gsm_arena_utils.py
+++ utils/gsm_arena_utils.py
@@ -1,16 +1,15 @@
 import json
 import requests as r
-from typing import Dict
+from typing import Any, Dict
 
 GSM_ARENA_API_URL = 'https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec'
 
 
-def get_from_gsm_arena(params: Dict[str, str], search: str = "") -> Dict:
+def get_from_gsm_arena(params: Dict[str, str], search: str = "") -> Dict[str, Any]:
  req = r.get(f"{GSM_ARENA_API_URL}{search}", params)
  return req.json()
 
 
-def post_to_gsm_arena(params: Dict[str, str]) -> Dict:
+def post_to_gsm_arena(params: Dict[str, str]) -> Dict[str, Any]:
  req = r.post(GSM_ARENA_API_URL, data=json.dumps(params))
- print(req)
  return req.json()
--- utils/json_utils.py
+++ utils/json_utils.py
@@ -1,7 +1,8 @@
 import dataclasses, json
+from typing import Any
 
 class EnhancedJSONEncoder(json.JSONEncoder):
-    def default(self, o):
+    def default(self, o: object) -> Any:
         if dataclasses.is_dataclass(o):
             return dataclasses.asdict(o)
         return super().default(o)
