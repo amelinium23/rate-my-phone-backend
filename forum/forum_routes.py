e3adbfc5202ad2899f63661790513d7c3d929c10
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-06-27 13:12:14 +0200

    all posts endpoint

--- forum/forum_routes.py
+++ forum/forum_routes.py
@@ -3,9 +3,21 @@ from typing import Any, Dict
 from flask import Response, request, current_app
 from forum import FORUM
 from dataclasses import asdict
-
 from forum.model.post import Post
 
+
+@FORUM.route('/post', methods=['GET'])
+def get_all_post() -> Response:
+  try:
+    data: Dict[str, Any] = json.loads(request.data)
+    user_uid: str = data.get('uid', '')
+    assert user_uid is not None, 'uid param is required'
+    db = current_app.config.get('FIRESTORE', None)
+    doc = db.collection('posts').document(user_uid).get().to_dict()
+    posts = doc.get('posts', [])
+    return Response(json.dumps(posts), status=200)
+  except Exception as e:
+    return Response(str(e), status=500)
 
 @FORUM.route('/post', methods=['POST'])
 def create_new_post() -> Response:
