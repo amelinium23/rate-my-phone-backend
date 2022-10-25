b23e7be9edd6df92f960e29979c29094ae5c834b
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-10-25 09:39:03 +0200

    remove allegro

--- allegro/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-
-ALLEGRO = Blueprint("allegro", __name__, url_prefix="/allegro")
--- allegro/model/search_mode.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class SearchMode(Enum):
-    REGULAR = "REGULAR"
-    DESCRIPTIONS = "DESCRIPTIONS"
-    CLOSED = "CLOSED"
